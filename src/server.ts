8e807fdc26223dc1ada689694c61249c71df957b
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-11-15 15:14:57 -0300

    chore: add express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3333
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
