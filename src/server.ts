d71df92bee3cc6f23d7a2fa1616be28075e68eba
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-11-15 15:40:30 -0300

    chore: add husky

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-const port = process.env.PORT || 3333
+const port = 3333
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
-  return res.json({ test: 'hello world' })
+  return res.json({ test: 'hello world' });
 })
 
 app.listen(port, () => {
