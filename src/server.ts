048fc19eca37be3eaff7292d0dc75d0768144f3c
Lucas Medeiros <lucaasmedeiros7@gmail.com>
2022-11-15 15:17:02 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3333
 
+app.use(cors())
 app.use(express.json())
+app.get('/', (req, res) => {
+  return res.json({ test: 'hello world' })
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
